f0e244b1b39f0850f82316f07f8439c7ecd67b99
KennyGuldentops <kenny_guldentops@msn.com>
2016-10-18 21:02:21 +0200

    Fixed jquery cdn probleem en servertest gefixt

--- servertest.js
+++ servertest.js
@@ -11,8 +11,12 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-
-
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 var randomcode = randomstring.generate(7);
