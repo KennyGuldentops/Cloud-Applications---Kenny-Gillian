5c73264a1f2c2e6f4ed5dae8b05c27c25d21d803
KennyGuldentops <kenny_guldentops@msn.com>
2016-10-24 09:04:16 +0200

    Server aangepast voor op AWS te draaien

--- servertest.js
+++ servertest.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var randomstring = require("randomstring");
 var fs = require("fs");
+var port  	 = process.env.PORT || 8080;
+var mongoose = require('mongoose'); 
+var database = require('./config/database'); 			// load the database config
+var morgan   = require('morgan');
+var methodOverride = require('method-override');
 var datani;
 var indatabase;
 var indatabaselesnaam;
@@ -11,12 +16,25 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
+// configuration ===============================================================
+mongoose.connect(database.url); 	// connect to mongoDB database on modulus.io
+
+app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
 
 
 var randomcode = randomstring.generate(7);
@@ -144,7 +162,7 @@ app.delete('/delete/:naam', function (req, res) {
 
 */
 
-var server = app.listen(5000, function () {
+var server = app.listen(port, function () {
 
   var host = server.address().address
   var port = server.address().port
